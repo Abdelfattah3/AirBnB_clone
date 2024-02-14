44939d34741b41d89f19110b237df6b82cb62b69
Abdelfattah3 <abdoselkady456@gmail.com>
2024-02-14 20:20:48 +0200

    task 5

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """reloading objects"""
 
-
+from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
+
+classes = {"BaseModel": BaseModel}
 
 storage = FileStorage()
 
