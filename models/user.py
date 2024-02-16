d69890ab4ccc41e48da9fc4bf9b7bff88775cbe6
Abdelfattah3 <abdoselkady456@gmail.com>
2024-02-16 15:03:35 +0200

    task 8

--- models/user.py
+++ models/user.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
 """User class for the user info"""
 
-from base_model import BaseModel
+from models.base_model import BaseModel
 
 
 class User(BaseModel):
     """User class for the user info"""
 
-    def __init__(self):
-        """initiate the class that inherit from basemodel"""
-        super().__init__(self)
-        self.email = ""
-        self.password = ""
-        self.first_nam = ""
-        self.last_name = ""
+    email = ""
+    password = ""
+    first_nam = ""
+    last_name = ""
